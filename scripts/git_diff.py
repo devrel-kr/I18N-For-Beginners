542074177a287a15605fd9d7a97a13917cc53c96
Eungu-Kwon <47956808+Eungu-Kwon@users.noreply.github.com>
2022-05-08 21:17:32 +0900

    fix typo

--- scripts/git_diff.py
+++ scripts/git_diff.py
@@ -47,7 +47,7 @@ def main(b1, b2):
 	files = get_files(b1, b2)
 	result = ""
 	for f in files:
-		if is_exist(b1, f[1]):
+		if is_exist(b2, f[1]):
 			diff = get_diff(b1, b2, f)
 			result += f'~ File: {diff["name"]}\n'
 			if diff['state'] == 'M':
