c52b866d3ba141572c8997a59f2999bedf938843
waroad <doha2012@naver.com>
2022-11-15 20:59:00 +0900

    mdpo initial commit

---
+++ mdpo/jsonFormating.py
@@ -0,0 +1,14 @@
+import json
+
+with open("mdpo/settings.json", "r") as json_file:
+    dict1 = json.load(json_file)
+    path_sum_sync=''
+    path_sum_mdpo=''
+    for k in dict1['path']:
+        path_sum_mdpo+='mdpo/'+k+' '
+        path_sum_sync+='^'+k+'|'
+    dict1["path_sum_sync"] = path_sum_sync[:-1]
+    dict1["path_sum_mdpo"] = path_sum_mdpo[:-1]
+    del dict1["path"]
+    with open("mdpo/settings_modified.json", "w") as json_w:
+        json.dump(dict1, json_w)
