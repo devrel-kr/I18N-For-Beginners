53d026a69d5fbf932898f0549ace1faf3f588838
YYun-D <85883811+YYun-D@users.noreply.github.com>
2022-11-19 19:29:32 +0900

    Add files via upload

---
+++ mdpo/scripts/weblate_install.sh
@@ -0,0 +1,34 @@
+# install docker & docker-compose
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Clone the weblate-docker repo
+git clone https://github.com/WeblateOrg/docker-compose.git weblate-docker
+cd weblate-docker
+
+# Create a docker-compose.override.yml file with your settings. See Docker environment variables for full list of environment variables.
+echo \
+"version: '3'
+services:
+  weblate:
+    ports:
+      - 80:8080
+    environment:
+      WEBLATE_EMAIL_HOST: smtp.example.com
+      WEBLATE_EMAIL_HOST_USER: user
+      WEBLATE_EMAIL_HOST_PASSWORD: pass
+      WEBLATE_SERVER_EMAIL: weblate@example.com
+      WEBLATE_DEFAULT_FROM_EMAIL: weblate@example.com
+      WEBLATE_SITE_DOMAIN: weblate.example.com
+      WEBLATE_ADMIN_PASSWORD: password for the admin user
+      WEBLATE_ADMIN_EMAIL: weblate.admin@example.com
+" > docker-compose.override.yml
+
+# Start Weblate containers
+sudo docker-compose up
